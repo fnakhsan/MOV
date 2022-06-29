9b51f9a4211d2399ee606683f035fd6b61f17414
fnakhsan <fn.akhsan@gmail.com>
2022-06-29 12:35:39 +0700

    fix error, but still ugly

--- app/src/main/java/com/fnakhsan/mov/MainActivity.kt
+++ app/src/main/java/com/fnakhsan/mov/MainActivity.kt
@@ -2,16 +2,19 @@ package com.fnakhsan.mov
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.fnakhsan.mov.onboard.OnboardOneActivity
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen()
-        startActivity(Intent(this@MainActivity, OnboardOneActivity::class.java))
-        finish()
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@MainActivity, OnboardOneActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
