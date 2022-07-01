d6297063c25b44fdcfa4476754faf66ebfe75a25
fnakhsan <fn.akhsan@gmail.com>
2022-07-01 20:59:07 +0700

    Movie Detail Activity Binding

--- app/src/main/java/com/fnakhsan/mov/MovieDetailActivity.kt
+++ app/src/main/java/com/fnakhsan/mov/MovieDetailActivity.kt
@@ -2,10 +2,13 @@ package com.fnakhsan.mov
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fnakhsan.mov.databinding.ActivityMovieDetailBinding
 
 class MovieDetailActivity : AppCompatActivity() {
+    private lateinit var movieDetailBinding: ActivityMovieDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie_detail)
+        movieDetailBinding = ActivityMovieDetailBinding.inflate(layoutInflater)
+        setContentView(movieDetailBinding.root)
     }
 }
