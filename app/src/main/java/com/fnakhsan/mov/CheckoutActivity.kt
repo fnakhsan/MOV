df1ea44967cf5cf123db567c671f163eef187992
fnakhsan <fn.akhsan@gmail.com>
2022-07-02 10:21:01 +0700

    Checkout Activity UI

---
+++ app/src/main/java/com/fnakhsan/mov/CheckoutActivity.kt
@@ -0,0 +1,14 @@
+package com.fnakhsan.mov
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fnakhsan.mov.databinding.ActivityCheckoutBinding
+
+class CheckoutActivity : AppCompatActivity() {
+    private lateinit var checkoutBinding: ActivityCheckoutBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        checkoutBinding = ActivityCheckoutBinding.inflate(layoutInflater)
+        setContentView(checkoutBinding.root)
+    }
+}
