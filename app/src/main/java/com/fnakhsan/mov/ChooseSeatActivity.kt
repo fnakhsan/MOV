26220fad45dc41f32dfd6e7a5dcafcda91ef04de
fnakhsan <fn.akhsan@gmail.com>
2022-07-02 09:55:48 +0700

    Choose Seat Activity UI

--- app/src/main/java/com/fnakhsan/mov/ChooseSeatActivity.kt
+++ app/src/main/java/com/fnakhsan/mov/ChooseSeatActivity.kt
@@ -2,10 +2,13 @@ package com.fnakhsan.mov
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fnakhsan.mov.databinding.ActivityChooseSeatBinding
 
 class ChooseSeatActivity : AppCompatActivity() {
+    private lateinit var chooseSeatBinding: ActivityChooseSeatBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_choose_seat)
+        chooseSeatBinding = ActivityChooseSeatBinding.inflate(layoutInflater)
+        setContentView(chooseSeatBinding.root)
     }
 }
