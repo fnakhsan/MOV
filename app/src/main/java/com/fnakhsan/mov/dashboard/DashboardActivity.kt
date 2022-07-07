1960d042cc786cf3a0970ee1a224f59d80119298
fnakhsan <fn.akhsan@gmail.com>
2022-07-07 21:48:52 +0700

    Add Dashboard Bottom Navigation

--- app/src/main/java/com/fnakhsan/mov/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/fnakhsan/mov/dashboard/DashboardActivity.kt
@@ -2,6 +2,12 @@ package com.fnakhsan.mov.dashboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import com.fnakhsan.mov.R
+import com.fnakhsan.mov.dashboard.home.HomeFragment
+import com.fnakhsan.mov.dashboard.setting.SettingFragment
+import com.fnakhsan.mov.dashboard.ticket.TicketFragment
 import com.fnakhsan.mov.databinding.ActivityDashboardBinding
 
 class DashboardActivity : AppCompatActivity() {
@@ -10,5 +16,35 @@ class DashboardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         dashboardBinding = ActivityDashboardBinding.inflate(layoutInflater)
         setContentView(dashboardBinding.root)
+        setFragment(HomeFragment())
+
+        with(dashboardBinding){
+            navHome.setOnClickListener{
+                setFragment(HomeFragment())
+                changeIcon(navHome, R.drawable.ic_home_active)
+                changeIcon(navTicket, R.drawable.ic_tiket)
+                changeIcon(navSetting, R.drawable.ic_profile)
+            }
+            navTicket.setOnClickListener {
+                setFragment(TicketFragment())
+                changeIcon(navHome, R.drawable.ic_home)
+                changeIcon(navTicket, R.drawable.ic_tiket_active)
+                changeIcon(navSetting, R.drawable.ic_profile)
+            }
+            navSetting.setOnClickListener {
+                setFragment(SettingFragment())
+                changeIcon(navHome, R.drawable.ic_home)
+                changeIcon(navTicket, R.drawable.ic_tiket)
+                changeIcon(navSetting, R.drawable.ic_profile_active)
+            }
+        }
+    }
+    private fun setFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.cv_fragment, fragment)
+    }
+    private fun changeIcon(imageView: ImageView, int: Int) {
+        imageView.setImageResource(int)
     }
 }
