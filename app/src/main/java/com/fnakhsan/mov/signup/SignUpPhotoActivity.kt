7f241477e48dc5d439c1cfbcb91ad827fe341517
fnakhsan <fn.akhsan@gmail.com>
2022-07-07 15:15:13 +0700

    Fix SignUpPhotoActivity

--- app/src/main/java/com/fnakhsan/mov/signup/SignUpPhotoActivity.kt
+++ app/src/main/java/com/fnakhsan/mov/signup/SignUpPhotoActivity.kt
@@ -4,11 +4,16 @@ import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -28,12 +33,23 @@ import java.util.*
 
 
 class SignUpPhotoActivity : AppCompatActivity(), PermissionListener {
-    val REQUEST_IMAGE_CAPTURE = 1
     private var statusAdd: Boolean = false
-    lateinit var filePath: Uri
-    lateinit var storage: FirebaseStorage
+    private lateinit var filePath: Uri
+    private lateinit var storage: FirebaseStorage
     lateinit var storageRef: StorageReference
     lateinit var preferences: Preferences
+    private var getImage =
+        registerForActivityResult(ActivityResultContracts.GetContent()) {
+            statusAdd = true
+
+            filePath = it!!
+            Glide.with(this)
+                .load(it)
+                .apply(RequestOptions.circleCropTransform())
+                .into(signUpPhotoBinding.photoProfile)
+            signUpPhotoBinding.btnSaveUpload.visibility = View.VISIBLE
+            signUpPhotoBinding.btnUpload.background = getDrawable(R.drawable.ic_btn_delete)
+        }
     private lateinit var signUpPhotoBinding: ActivitySignUpPhotoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -108,7 +124,7 @@ class SignUpPhotoActivity : AppCompatActivity(), PermissionListener {
     override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
             takePictureIntent.resolveActivity(packageManager)?.also {
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                getImage.launch("image/*")
             }
         }
     }
@@ -125,19 +141,8 @@ class SignUpPhotoActivity : AppCompatActivity(), PermissionListener {
 
     }
 
-    @Deprecated("Deprecated in Java")
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
-            val bitmap = data?.extras?.get("data") as Bitmap
-            statusAdd = true
-
-            filePath = data.data!!
-            Glide.with(this)
-                .load(bitmap)
-                .apply(RequestOptions.circleCropTransform())
-                .into(signUpPhotoBinding.photoProfile)
-            signUpPhotoBinding.btnSaveUpload.visibility = View.VISIBLE
-            signUpPhotoBinding.btnUpload.background = getDrawable(R.drawable.ic_btn_delete)
-        }
+    companion object {
+        private const val TAG = "Photo"
     }
+
 }
