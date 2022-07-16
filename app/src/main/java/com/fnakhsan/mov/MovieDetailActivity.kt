5a7189a0740d717f806fd3740ba44567b3072832
fnakhsan <fn.akhsan@gmail.com>
2022-07-16 20:04:04 +0700

    Add UI MovieDetailActivity.kt

--- app/src/main/java/com/fnakhsan/mov/MovieDetailActivity.kt
+++ app/src/main/java/com/fnakhsan/mov/MovieDetailActivity.kt
@@ -2,6 +2,9 @@ package com.fnakhsan.mov
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.fnakhsan.mov.data.Film
 import com.fnakhsan.mov.databinding.ActivityMovieDetailBinding
 
 class MovieDetailActivity : AppCompatActivity() {
@@ -10,5 +13,17 @@ class MovieDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         movieDetailBinding = ActivityMovieDetailBinding.inflate(layoutInflater)
         setContentView(movieDetailBinding.root)
+
+        val data = intent.getParcelableExtra<Film>("film")
+
+        with(movieDetailBinding){
+            tvTitle.text = data?.genre
+            tvGenre.text = data?.genre
+            tvRate.text = data?.rating
+            Glide.with(this@MovieDetailActivity)
+                .load(data?.poster)
+                .apply(RequestOptions.centerCropTransform())
+                .into(ivCover)
+        }
     }
 }
