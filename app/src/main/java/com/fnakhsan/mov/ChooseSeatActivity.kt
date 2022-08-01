1bbca12984f741f8645c5f93fe2187925715ac45
fnakhsan <fn.akhsan@gmail.com>
2022-08-01 10:07:25 +0700

    Assign Variable

--- app/src/main/java/com/fnakhsan/mov/ChooseSeatActivity.kt
+++ app/src/main/java/com/fnakhsan/mov/ChooseSeatActivity.kt
@@ -7,11 +7,21 @@ import com.fnakhsan.mov.databinding.ActivityChooseSeatBinding
 
 class ChooseSeatActivity : AppCompatActivity() {
     private lateinit var chooseSeatBinding: ActivityChooseSeatBinding
+    private var total: Int = 0
+    private lateinit var seats : MutableList<Boolean>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         chooseSeatBinding = ActivityChooseSeatBinding.inflate(layoutInflater)
         setContentView(chooseSeatBinding.root)
 
         intent.getParcelableExtra<Film>("data")
+        seatInit()
+    }
+
+    fun seatInit(){
+        for(i in 0..29){
+            seats[i] = false
+        }
     }
 }
