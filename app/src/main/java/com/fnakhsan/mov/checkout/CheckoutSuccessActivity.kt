8f40b49d03b4929343991cae6380ead4e17bbad2
fnakhsan <fn.akhsan@gmail.com>
2022-08-23 07:43:19 +0700

    Add intent

--- app/src/main/java/com/fnakhsan/mov/checkout/CheckoutSuccessActivity.kt
+++ app/src/main/java/com/fnakhsan/mov/checkout/CheckoutSuccessActivity.kt
@@ -1,12 +1,28 @@
 package com.fnakhsan.mov.checkout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.fnakhsan.mov.R
+import com.fnakhsan.mov.dashboard.home.HomeFragment
+import com.fnakhsan.mov.dashboard.ticket.TicketFragment
+import com.fnakhsan.mov.databinding.ActivityCheckoutSuccessBinding
 
 class CheckoutSuccessActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCheckoutSuccessBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_checkout_success)
+        binding = ActivityCheckoutSuccessBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            btnTicket.setOnClickListener {
+                val intent = Intent(this@CheckoutSuccessActivity, TicketFragment::class.java)
+                startActivity(intent)
+            }
+            btnHome.setOnClickListener {
+                val intent = Intent(this@CheckoutSuccessActivity, HomeFragment::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
